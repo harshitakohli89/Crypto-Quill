bbc1316e8175e54448694a7b15106b8c2d201de6
Harshita kohli <harshitakohli89@gmail.com>
2024-07-29 20:49:39 +0530

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
-  input: 'src/index.js', 
+  input: 'src/main.js', 
   output: {
     file: 'dist/bundle.js',
     format: 'cjs'
